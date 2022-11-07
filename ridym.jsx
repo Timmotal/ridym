4bcc8f59e1a0563f283b12c139559afc30a7663e
move style to head <timmortal44@yahoo.com>
2022-11-08 00:28:46 +0100

    update file name

---
+++ ridym.jsx
@@ -0,0 +1,5 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
