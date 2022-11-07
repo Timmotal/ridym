d35c9c05b1a283c0546341a59eee272260673f64
move style to head <timmortal44@yahoo.com>
2022-11-08 00:28:17 +0100

    update file name

--- App.jsx
+++
@@ -1,5 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
