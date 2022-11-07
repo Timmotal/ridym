9c5a6eeacadf9a33647d599defa1d41d116cae5e
move style to head <timmortal44@yahoo.com>
2022-11-08 00:00:56 +0100

    import chart, top charts discover

--- App.jsx
+++ App.jsx
@@ -2,3 +2,4 @@ import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
