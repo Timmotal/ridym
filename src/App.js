e91d3f3ada522468d8b4b4ba7016305ffe8a0d6c
move style to head <timmortal44@yahoo.com>
2022-11-08 23:46:33 +0100

    add inner div to app wrapper

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+    
+
+       
+      </div>
+
+    
     </div>
   );
 };
