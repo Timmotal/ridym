0c6e0b413c827214a2103098c95c5f27dbcd631f
move style to head <timmortal44@yahoo.com>
2022-11-09 00:03:04 +0100

    make const artistCard

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,22 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 const ArtistCard = ({ track }) => {
-  const navigate = useNavigate();
 
-  return (
-    <div 
-    className="flex flex-col w p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate slideup rounded-lg cursor-pointer"
-    onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
-    // broo damn one of the most confusing things i've had to struggle to comprehend
-    //  how to make each link be specific to each card
-    //   the answer lies in the loop bro and check it out just above
-    >
-      <img src={track?.images?.coverart} alt="" className="w-full h-56 rounded-lg" />
-      <p className="mt-4 text-white font-semibold text-lg truncate">
-        {track?.subtitle}
-      </p>
-    </div>
-  )
+
+  
 };
-
-export default ArtistCard;
+\\
