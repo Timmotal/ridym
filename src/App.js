12c38f16654c9d9e1517aeb8a566852b34884b19
move style to head <timmortal44@yahoo.com>
2022-11-08 23:51:02 +0100

    add songs details routes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
               <Route path="/top-charts" element={<TopCharts />} />
               <Route path="/around-you" element={<AroundYou />} />
               <Route path="/artists/:id" element={<ArtistDetails />} />
-           
+              <Route path="/songs/:songid" element={<SongDetails />} />
               {/* i need to add a isFetching messgae for the lyrics component, im clicking at he wrong place, click at the title andparagraspgh */}
          
             </Routes>
