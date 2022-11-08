4e68266584ed454aa70df2441c0aaa2cda04435f
move style to head <timmortal44@yahoo.com>
2022-11-08 23:47:51 +0100

    addanother div and hide the scroll

--- src/App.js
+++ src/App.js
@@ -14,7 +14,10 @@ const App = () => {
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
-       
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        
+         
+        </div>
       </div>
 
     
