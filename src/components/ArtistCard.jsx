c55bde53582729f86cce8a0c3bd4e38c91e0fd5f
move style to head <timmortal44@yahoo.com>
2022-11-09 00:03:38 +0100

    return div

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -3,6 +3,13 @@ import { useNavigate } from "react-router-dom";
 const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
 
-  
+  return (
+    <div 
+    className="flex flex-col w p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate slideup rounded-lg cursor-pointer"
+    onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+
+    >
+   
+  )
 };
 \\
