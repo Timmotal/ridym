0b9a1e0e46a739f12491fe0591e1af759318b0b7
move style to head <timmortal44@yahoo.com>
2022-11-08 23:46:50 +0100

    add search bar component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <div className="relative flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
-    
+        <Searchbar />
 
        
       </div>
