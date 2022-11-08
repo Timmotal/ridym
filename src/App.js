1e955e9ce5c9e3d771f161a1e534652045d4c92c
move style to head <timmortal44@yahoo.com>
2022-11-08 23:51:43 +0100

    add sticky div

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ const App = () => {
               <Route path="/search/:searchTerm" element={<Search />} />
             </Routes>
           </div>
-         
+          <div className="xl:sticky relative top-0 h-fit">
+        
+          </div>
         </div>
       </div>
 
