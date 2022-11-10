26269bf2c8f6d93721eaacfb0410230d62f62a3f
move style to head <timmortal44@yahoo.com>
2022-11-10 23:59:02 +0100

    add onclick to ms skip prevoius icon

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -5,7 +5,7 @@ import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react
 const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }) => (
   <div className="flex items-center justify-around md:w-36 lg:w-52 2xl:w-80">
     <BsArrowRepeat size={20} color={repeat ? 'red' : 'white'} onClick={() => setRepeat((prev) => !prev)} className="hidden sm:block cursor-pointer" />
-    {currentSongs?.length && <MdSkipPrevious size={30} color="#FFF" className="cursor-pointer" />}
+    {currentSongs?.length && <MdSkipPrevious size={30} color="#FFF" className="cursor-pointer" onClick={handlePrevSong} />}
     {isPlaying ? (
    
    
