9f039c4d402482976e71b5799499706c20012bac
move style to head <timmortal44@yahoo.com>
2022-11-10 23:55:37 +0100

    import icons for music comp[ponents

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdSkipNext, MdSkipPrevious } from 'react-icons/md';
-
+import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
 
 
 
