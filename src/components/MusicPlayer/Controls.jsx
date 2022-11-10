ca96bdb8fe4ea89f3fb2cec659b68bc1fe4553ad
move style to head <timmortal44@yahoo.com>
2022-11-10 23:56:18 +0100

    pass iin props into control

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MdSkipNext, MdSkipPrevious } from 'react-icons/md';
 import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
 
-const Controls = () => (
+const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }) => (
  
 );
 
