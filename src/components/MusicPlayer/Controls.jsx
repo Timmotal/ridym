22c907c6eeda60788c6aa0c204630aa1e228fa97
move style to head <timmortal44@yahoo.com>
2022-11-11 00:17:30 +0100

    add Bs Shuffle to MusicPlayer

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -11,8 +11,8 @@ const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSo
     ) : (
       <BsFillPlayFill size={45} color="#FFF" onClick={handlePlayPause} className="cursor-pointer" />
     )}
-
-   
+    {currentSongs?.length && <MdSkipNext size={30} color="#FFF" className="cursor-pointer" onClick={handleNextSong} />}
+    <BsShuffle  />
   </div>
 );
 
