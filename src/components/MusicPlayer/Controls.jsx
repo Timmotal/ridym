c120e3492200e28521a83dbf2ba98b2034efa553
move style to head <timmortal44@yahoo.com>
2022-11-10 23:56:40 +0100

    add div into controls

--- src/components/MusicPlayer/Controls.jsx
+++ src/components/MusicPlayer/Controls.jsx
@@ -3,7 +3,14 @@ import { MdSkipNext, MdSkipPrevious } from 'react-icons/md';
 import { BsArrowRepeat, BsFillPauseFill, BsFillPlayFill, BsShuffle } from 'react-icons/bs';
 
 const Controls = ({ isPlaying, repeat, setRepeat, shuffle, setShuffle, currentSongs, handlePlayPause, handlePrevSong, handleNextSong }) => (
- 
+  <div className="flex items-center justify-around md:w-36 lg:w-52 2xl:w-80">
+  
+   
+  
+   
+   
+   
+  </div>
 );
 
 
