2e3189c3b28bc7d7ebe0d1018e791af1b67614ad
move style to head <timmortal44@yahoo.com>
2022-11-14 21:42:43 +0100

    ini volume to useState

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -13,7 +13,7 @@ const MusicPlayer = () => {
   const [duration, setDuration] = useState(0);
   const [seekTime, setSeekTime] = useState(0);
   const [appTime, setAppTime] = useState(0);
- 
+  const [volume, setVolume] = useState(0.3);
 
 
 
