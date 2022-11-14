76300946720eaa9a30305c4107d0bceaace2481a
move style to head <timmortal44@yahoo.com>
2022-11-14 21:42:27 +0100

    initialize apptimeand setApptime to usestate

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -12,7 +12,7 @@ const MusicPlayer = () => {
   const { activeSong, currentSongs, currentIndex, isActive, isPlaying } = useSelector((state) => state.player);
   const [duration, setDuration] = useState(0);
   const [seekTime, setSeekTime] = useState(0);
-
+  const [appTime, setAppTime] = useState(0);
  
 
 
