d24eb27c2ddbf4c6955b6681779ab29b5087c215
move style to head <timmortal44@yahoo.com>
2022-11-14 21:49:29 +0100

    add controls comp

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -57,7 +57,13 @@ const MusicPlayer = () => {
     <div className="relative sm:px-12 px-8 w-full flex items-center justify-between">
       <Track isPlaying={isPlaying} isActive={isActive} activeSong={activeSong} />
       <div className="flex-1 flex flex-col items-center justify-center">
-      
+        <Controls
+       
+  
+    
+        
+        
+        />
       
       
       </div>
