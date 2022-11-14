f9000bca60813ef90f83207ea64db9378933b49c
move style to head <timmortal44@yahoo.com>
2022-11-14 21:38:07 +0100

    import nexSong, prevSongand playpause component

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-
+import { nextSong, prevSong, playPause } from '../../redux/features/playerSlice';
 
 
 
