a712e1557d6f7e298b2c4d09b1ab7632730f76f5
move style to head <timmortal44@yahoo.com>
2022-11-14 21:38:19 +0100

    import controls

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { nextSong, prevSong, playPause } from '../../redux/features/playerSlice';
+import Controls from './Controls';
 
 
 
