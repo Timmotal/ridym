19de26f8435c09c4b9e97909c68a4e2c0b92bbbf
move style to head <timmortal44@yahoo.com>
2022-11-15 23:47:38 +0100

    pass in props for player comp

--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useRef, useEffect } from 'react';
 
+const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate, onLoadedData, repeat }) => {
+ 
+
+  // updates audio element only on seekTime change (and not on each rerender):
+ 
+ 
+};
 
 
