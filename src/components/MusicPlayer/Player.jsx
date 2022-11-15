f5edb2057fd8ca498cab18aadd7c7bc7bbde63b1
move style to head <timmortal44@yahoo.com>
2022-11-15 23:48:49 +0100

    add if condition for ref current

--- src/components/MusicPlayer/Player.jsx
+++ src/components/MusicPlayer/Player.jsx
@@ -4,7 +4,9 @@ import React, { useRef, useEffect } from 'react';
 const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate, onLoadedData, repeat }) => {
   const ref = useRef(null);
   // eslint-disable-next-line no-unused-expressions
-  i
+  if (ref.current) {
+   
+  }
 
 
   // updates audio element only on seekTime change (and not on each rerender):
