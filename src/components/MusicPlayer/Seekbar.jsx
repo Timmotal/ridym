6c9f4c80338942b64939e2f520b1649c7a4a41aa
move style to head <timmortal44@yahoo.com>
2022-11-16 23:43:06 +0100

    add type, step, and value props to input

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -11,7 +11,9 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
       </button>
       <p className="text-white">{value === 0 ? '0:00' : getTime(value)}</p>
       <input
-    
+        type="range"
+        step="any"
+        value={value}
     
 
       />
