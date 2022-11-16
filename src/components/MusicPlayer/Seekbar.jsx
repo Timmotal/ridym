1507565eee9d310efdb40dd4171de4231cad7704
move style to head <timmortal44@yahoo.com>
2022-11-16 23:41:45 +0100

    make a return

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -4,6 +4,8 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
   // converts the time to format 0:00
   const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
 
- 
+  return (
+    
+  );
 };
 
