3ed80effd94ff3f8a0b9146d11162e2f7f7355ef
move style to head <timmortal44@yahoo.com>
2022-11-16 23:41:36 +0100

    get time andformat properly

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
-
+  // converts the time to format 0:00
+  const getTime = (time) => `${Math.floor(time / 60)}:${(`0${Math.floor(time % 60)}`).slice(-2)}`;
 
  
 };
