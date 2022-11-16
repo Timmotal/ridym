e334587c0355e0e652af0960c7e262ee10423d9c
move style to head <timmortal44@yahoo.com>
2022-11-16 23:43:53 +0100

    add another paragraph

--- src/components/MusicPlayer/Seekbar.jsx
+++ src/components/MusicPlayer/Seekbar.jsx
@@ -19,7 +19,8 @@ const Seekbar = ({ value, min, max, onInput, setSeekTime, appTime }) => {
         onInput={onInput}
         className="md:block w-24 md:w-56 2xl:w-96 h-1 mx-4 2xl:mx-6 rounded-lg"
       />
-  
+      <p className="text-white">{max === 0 ? '0:00' : getTime(max)}</p>
+    
     </div>
   );
 };
