525ba4be6170f2c255f84a63d5cc1aea87e27072
move style to head <timmortal44@yahoo.com>
2022-11-18 00:01:04 +0100

    add div

--- src/components/MusicPlayer/VolumeBar.jsx
+++ src/components/MusicPlayer/VolumeBar.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'react-icons/bs';
 
 const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
+  <div className="hidden lg:flex flex-1 items-center justify-end">
 
+
+
+   
+  </div>
 );
 
