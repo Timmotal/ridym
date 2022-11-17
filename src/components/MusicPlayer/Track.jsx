79c705c009dabe491eb74604b51e68ccab8b41d7
move style to head <timmortal44@yahoo.com>
2022-11-17 23:52:17 +0100

    add anither div

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Track = ({ isPlaying, isActive, activeSong }) => (
   <div className="flex-1 flex items-center justify-start">
-    
+    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
+     
+    </div>
   
   </div>
 );
