4100e18582cb7faa329c9a3fdc8cc665090593ad
move style to head <timmortal44@yahoo.com>
2022-11-17 23:52:30 +0100

    add img comp

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Track = ({ isPlaying, isActive, activeSong }) => (
   <div className="flex-1 flex items-center justify-start">
     <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
-     
+      <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
     </div>
   
   </div>
