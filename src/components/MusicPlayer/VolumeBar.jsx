bbc4747b33afb4954fcd5bf4542c640628d6c355
move style to head <timmortal44@yahoo.com>
2022-11-18 00:00:52 +0100

    descontruct props

--- src/components/MusicPlayer/VolumeBar.jsx
+++ src/components/MusicPlayer/VolumeBar.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
 import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'react-icons/bs';
 
+const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
+
+);
 
