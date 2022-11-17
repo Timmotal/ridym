892d3795a340b15ab00a0e4b33d49c0d13a42697
move style to head <timmortal44@yahoo.com>
2022-11-17 23:52:44 +0100

    aded third div

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -5,7 +5,10 @@ const Track = ({ isPlaying, isActive, activeSong }) => (
     <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
       <img src={activeSong?.images?.coverart} alt="cover art" className="rounded-full" />
     </div>
-  
+    <div className="w-[50%]">
+    
+    
+    </div>
   </div>
 );
 
