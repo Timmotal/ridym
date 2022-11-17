29d5c93b18ca756b887cea6e1fbbfe2d4f16ebcb
move style to head <timmortal44@yahoo.com>
2022-11-18 00:02:21 +0100

    add conditon for volume uo

--- src/components/MusicPlayer/VolumeBar.jsx
+++ src/components/MusicPlayer/VolumeBar.jsx
@@ -3,7 +3,7 @@ import { BsFillVolumeUpFill, BsVolumeDownFill, BsFillVolumeMuteFill } from 'reac
 
 const VolumeBar = ({ value, min, max, onChange, setVolume }) => (
   <div className="hidden lg:flex flex-1 items-center justify-end">
-
+    {value <= 1 && value > 0.5 && <BsFillVolumeUpFill size={25} color="#FFF" onClick={() => setVolume(0)} />}
 
 
    
