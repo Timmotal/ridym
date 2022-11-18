d675f5902adaf8997e90ebc49c21477d9ab1e6d6
move style to head <timmortal44@yahoo.com>
2022-11-18 23:21:18 +0100

    add another div

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -6,7 +6,12 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
 
   return (
   <div className="relative w-full flex flex-col">
-    
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28"/>
+
+ 
+ 
+    {/* <div>DetailsHeader</div> */}
+  </div>
   )};
 
 
