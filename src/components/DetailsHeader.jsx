a2ecbcd04b21e274f2a624d81fa491e346f5f0e1
move style to head <timmortal44@yahoo.com>
2022-11-18 23:17:09 +0100

    initialize data for artists

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -2,3 +2,9 @@ import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => {
 
+  const artist = artistData?.artists[artistId].attributes; // so to reduce the long string and make it data simpler we did this
+
+  return (
+ 
+
+
