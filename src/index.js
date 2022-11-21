a299555b16f2a065282c50e2e8bfb2dc91a04909
move style to head <timmortal44@yahoo.com>
2022-11-22 00:02:20 +0100

    import ArtistCard;

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import Sidebar from './Sidebar';
 import Searchbar from './Searchbar';
 import SongCard from './SongCard';
 import TopPlay from './TopPlay';
-
-
+import ArtistCard from './ArtistCard';
+import DetailsHeader from './DetailsHeader';
 
 
 
