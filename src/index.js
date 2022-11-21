095229b32d12f11c0fe1360f7040756b0412ed2b
move style to head <timmortal44@yahoo.com>
2022-11-22 00:03:26 +0100

    import erorr comp

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import SongBar from './SongBar';
 import RelatedSongs from './RelatedSongs';
 import MusicPlayer from './MusicPlayer';
 import Loader from './Loader';
-;
+import Error from './Error';
 
 export {
 
