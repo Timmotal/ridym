dc05ff0f26efa52d7719ba001826131dd6cc588a
move style to head <timmortal44@yahoo.com>
2022-11-24 23:34:26 +0100

    add image and render

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { loader } from '../assets'
 
 const Loader = ({ title }) => (
   <div className="w-full flex justify-center items center flex-col">
-
+    <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
 
   </div>
 );
