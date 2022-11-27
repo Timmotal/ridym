b7fc646e47994e65b417786b23802f29106f6ecb
move style to head <timmortal44@yahoo.com>
2022-11-27 21:43:13 +0100

    add h1 to Rsngs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -2,7 +2,9 @@ import SongBar from "./SongBar";
 
 const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId }) => (
   <div className="flex flex-col">
-  
+    <h1 className="font-bold text-3xl text-white">
+    
+    </h1>
 
    
   </div>
