dd1dee0ea117a97d6e2a477494af6f5e5c22bc30
move style to head <timmortal44@yahoo.com>
2022-11-27 21:33:28 +0100

    add class to play

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -10,7 +10,7 @@ const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => 
                           ) : (
                              <FaPlayCircle 
                              size={35}
-                        
+                             className="text-gray-300"
                             
                              /> 
                              ))
