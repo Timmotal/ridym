2c448d11c0b873a1f5cac0e75823fa7562e554ea
move style to head <timmortal44@yahoo.com>
2022-11-27 21:51:49 +0100

    add searchBar const

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,5 +2,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 
+const Searchbar = () => {
+ 
+ 
+
+ 
+  return(
+  
+)};
 
 
