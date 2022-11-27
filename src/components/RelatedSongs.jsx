647323594e205fb47c2fff8b0e85fc11f4673dee
move style to head <timmortal44@yahoo.com>
2022-11-27 21:42:58 +0100

    add div

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,7 +1,11 @@
 import SongBar from "./SongBar";
 
 const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId }) => (
+  <div className="flex flex-col">
   
+
+   
+  </div>
   // <div>Loader</div>
 );
 
