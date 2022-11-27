e100fc432eb66ec97dc5454f8949c06aa3c1830e
move style to head <timmortal44@yahoo.com>
2022-11-27 21:32:05 +0100

    passin class to pause

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -4,7 +4,7 @@ const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => 
                       ( isPlaying && activeSong?. title === song.title ? // if we are playing and active song is equal to a particular song
                         ( <FaPauseCircle
                           size={35}
-                      
+                          className="text-gray-300"
                         
                           /> 
                           ) : 
