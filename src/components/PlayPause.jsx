a5021c29a53e0b82defa423107d47d1aacc2e358
move style to head <timmortal44@yahoo.com>
2022-11-27 21:31:00 +0100

    assign playPause const

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,4 +1,5 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
-
+// why simple handlePause and not handlePauseClick
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => // if i had traced the props maybe i would have figured
 
 
