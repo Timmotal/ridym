901382df8caf6f865cddd63bd8799e3ee7d5b20c
move style to head <timmortal44@yahoo.com>
2022-11-27 21:54:24 +0100

    add label into form

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -17,7 +17,9 @@ const Searchbar = () => {
   onSubmit={handleSubmit}
    autoComplete="off" 
    className="p-2 text-gray-400 focus-within:text-gray-600">
-    
+    <label htmlFor="search-field" className="sr-only">
+  
+    </label>
   
   </form>
 )};
