20a6bb9df2628a320624fc229d81a74c0ddc25bc
move style to head <timmortal44@yahoo.com>
2022-11-27 21:43:24 +0100

    add text to h1

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -3,7 +3,7 @@ import SongBar from "./SongBar";
 const RelatedSongs = ({ data, isPlaying, activeSong, handlePauseClick, handlePlayClick, artistId }) => (
   <div className="flex flex-col">
     <h1 className="font-bold text-3xl text-white">
-    
+      Related Songs
     </h1>
 
    
