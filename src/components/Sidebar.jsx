85b5625475259e567c8720fe64c5984b33efe6ab
move style to head <timmortal44@yahoo.com>
2022-11-28 23:19:07 +0100

    import hiOutline

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { RiCloseLine } from 'react-icons/ri';
-
+import { HiOutlineMenu } from "react-icons/hi";
 
 
 
