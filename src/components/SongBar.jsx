cd3dc6f1bba888c30b5f5c3bb1a954f6eae7b398
move style to head <timmortal44@yahoo.com>
2022-11-30 23:30:20 +0100

    add p tag

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -15,7 +15,9 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
       <div className="flex-1 flex flex-col justify-center mx-3">
         {!artistId ? (
           <Link to={`/songs/${song.key}`}>
-           
+            <p className="text-xl font-bold text-white">
+       
+            </p>
           </Link>
         ) : (
         
