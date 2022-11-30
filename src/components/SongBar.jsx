56238efa1a86093eebfbfa6e92589fec41bbc9c6
move style to head <timmortal44@yahoo.com>
2022-11-30 23:27:50 +0100

    add div

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import PlayPause from './PlayPause';
 
 const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
- 
+  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+
+   
+   
+  </div>
 );
 
