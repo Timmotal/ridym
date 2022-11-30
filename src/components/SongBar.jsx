a9f57c30a7ecb8e0e4d40e7a423dd233bf8c0ada
move style to head <timmortal44@yahoo.com>
2022-11-30 23:28:53 +0100

    add src to image

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -9,7 +9,8 @@ const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, h
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
-       />
+        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart}
+      />
      
     </div>
    
