e067fe3db65fab9fb530da4e465f11d447ca3dad
move style to head <timmortal44@yahoo.com>
2022-12-03 23:07:55 +0100

    assignn dispathc to variable

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -8,7 +8,8 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
   // const activeSong = 'Test';
 
- 
+  const dispatch = useDispatch();
+
  
 
   // why are we not also passing the argument into the function here like we did in TopPlay
