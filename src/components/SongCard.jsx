c12402ec08b83174105c001a7f5df1804deb4c45
move style to head <timmortal44@yahoo.com>
2022-12-03 23:08:18 +0100

    make handlePauseClick Fn

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -10,7 +10,9 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
 
   const dispatch = useDispatch();
 
- 
+  const handlePauseClick = () => { // why are we not passing (song and i) params into here ?
+   
+  };
 
   // why are we not also passing the argument into the function here like we did in TopPlay
   
