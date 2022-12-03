2a13159d180684a2e3955b900c7fe2ba44605551
move style to head <timmortal44@yahoo.com>
2022-12-03 23:07:10 +0100

    import playPause from redux playerSlice

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import PlayPause from './PlayPause';
-
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 
 
