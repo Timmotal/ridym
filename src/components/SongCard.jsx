d47c295532be318798f990a45013eadd38b0b7cd
move style to head <timmortal44@yahoo.com>
2022-12-03 23:18:40 +0100

    check for active song

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -27,7 +27,7 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
     <div className="relative w-full h-56 group">
     <div 
         className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
-        
+         ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'} `}>
           
          </div>
          
