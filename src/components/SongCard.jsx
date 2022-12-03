e98b9b8e49206d459023e6597221b5507beb344d
move style to head <timmortal44@yahoo.com>
2022-12-03 23:07:23 +0100

    make comp songCard

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,5 +5,17 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 
+const SongCard = ({ ) => {
+  // const activeSong = 'Test';
 
+ 
+ 
+
+  // why are we not also passing the argument into the function here like we did in TopPlay
+  
+
+  // console.log(song)
+
+ 
+};
 
