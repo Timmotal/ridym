f0d103fa0ca0a1a6e18a2712b57ceaa872130e5c
move style to head <timmortal44@yahoo.com>
2022-12-03 23:13:24 +0100

    make inner div

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -24,7 +24,14 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
 
   return (
   <div className="flex flex-col p-4 bg-white/5 w-[250px] bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-   >
+    <div className="relative w-full h-56 group">
+   
+         
+         
+
+    
+        
+    </div>
 
    
   </div>
