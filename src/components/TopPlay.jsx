f78e3bf76051fa2567b6e703a33b5dd7b46d532d
move style to head <timmortal44@yahoo.com>
2022-12-04 16:28:40 +0100

    import freeMode

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useSelector,useDispatch } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { FreeMode } from 'swiper';
 
 
 
