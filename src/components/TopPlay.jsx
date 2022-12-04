65af8df4f20762ed3b66641dddedbe199386be62
move style to head <timmortal44@yahoo.com>
2022-12-04 16:29:02 +0100

    import setActive song

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper';
 
 import PlayPause from './PlayPause';
-
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 
 
