15e5d0fba5998443965615b9e6c5fa3e3ec40dde
move style to head <timmortal44@yahoo.com>
2022-12-04 17:04:59 +0100

    import getTopChartQuery from redux

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
-
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
 
 
