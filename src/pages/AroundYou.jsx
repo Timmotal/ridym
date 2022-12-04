ac088c48ea346944ea3e478c8e01eeb4b6d30fbb
move style to head <timmortal44@yahoo.com>
2022-12-04 17:04:01 +0100

    import Error and Loadeer and SongCard comp

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect} from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-
+import { Error, Loader, SongCard } from "../components";
 
 
 
