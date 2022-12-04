cf37c5b83ba9160eb8d1b969651ee69c75bef236
move style to head <timmortal44@yahoo.com>
2022-12-04 16:33:10 +0100

    add p tag to link el

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -20,7 +20,9 @@ const TopChartCard = ({ song, i, isPlaying, handlePauseClick, handlePlayClick, a
       <img src={song?.images?.coverart} alt={song?.title} className="w-20 h-20 rounded-lg" />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
-        
+          <p className="text-xl font-bold text-white">
+         
+          </p>
         </Link>
 
       
