094d3d658f5461607b8dc683ac5df390d93fea9a
move style to head <timmortal44@yahoo.com>
2022-12-04 16:41:12 +0100

    make inner div in return

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -78,7 +78,10 @@ const TopPlay = () => {
 
   return (
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+    
      
+      </div>
       
       
     </div>
