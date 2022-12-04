5e770ce4c40ebf7711e7e91f92916900097db5e9
move style to head <timmortal44@yahoo.com>
2022-12-04 16:41:54 +0100

    make h2 tag inside of the inner div div

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -80,7 +80,7 @@ const TopPlay = () => {
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
       <div className="flex flex-row justify-between items-center">
-      
+        <h2 className="text-white font-bold text-2xl">Top Charts</h2>
        
       </div>
      
