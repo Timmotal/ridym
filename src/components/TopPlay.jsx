8c063436f9dfc1cabafc8b159c4143f27536dba9
move style to head <timmortal44@yahoo.com>
2022-12-04 16:33:29 +0100

    render song title

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -21,7 +21,7 @@ const TopChartCard = ({ song, i, isPlaying, handlePauseClick, handlePlayClick, a
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
           <p className="text-xl font-bold text-white">
-         
+            {song?.title}
           </p>
         </Link>
 
