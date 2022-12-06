e9788795f080751e318437b7547a8d0de081b3cc
move style to head <timmortal44@yahoo.com>
2022-12-06 23:49:46 +0100

    imp topCharts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import TopArtists from './TopArtists';
 import ArtistDetails from './ArtistDetails';
 import SongDetails from './SongDetails';
 import Search from './Search';
+import TopCharts from './TopCharts';
 
 
 export {
