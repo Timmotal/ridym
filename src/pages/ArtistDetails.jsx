013b46ff6a6d262ce8b5575030a38f6450464ce2
move style to head <timmortal44@yahoo.com>
2022-12-06 23:26:59 +0100

    import details header

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom"; 
+import { useParams } from "react-router-dom"; //this gives us access to the id of the url
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 
 //SO MUCH TO BREAKDOWN AND UNDERSTAND HERE
