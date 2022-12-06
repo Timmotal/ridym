973680cc466e76f11de78022b509751620966391
move style to head <timmortal44@yahoo.com>
2022-12-06 23:35:25 +0100

    import useGetSOngsByGenreQuery

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,6 +3,12 @@ import { selectGenreListId } from "../redux/features/playerSlice";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 // import { useGetSongsByGenreQuery } from "../redux/services/shazamCore"; now we want to use the Genre
+import {useGetSongsByGenreQuery } from "../redux/services/shazamCore";
+
+// remember tutor says:
+// we use "SELECT" when we want to select a piece of "STATE"
+// "DISPATCH" when we want to modify the "STATE"
+// with a SELECTOR we will be able to fetch that modified STATE
 
 
 
