d00e76eff99938afb005dcb21f32fdacb44ba41f
move style to head <timmortal44@yahoo.com>
2022-12-06 23:27:13 +0100

    import useGetArtistDetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom"; //this gives us access to the id of the url
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
-
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
 
 //SO MUCH TO BREAKDOWN AND UNDERSTAND HERE
 
