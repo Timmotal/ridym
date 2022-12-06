3a6646d6dbff0b33c1c1633a04dbf84d95a7d86e
move style to head <timmortal44@yahoo.com>
2022-12-06 23:34:58 +0100

    import error loader and songcard

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectGenreListId } from "../redux/features/playerSlice";
+import { Error, Loader, SongCard } from "../components";
 e
 
 
