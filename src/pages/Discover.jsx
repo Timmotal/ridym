173d902bd187f4dd34b883b9979f3f1ce90f8045
move style to head <timmortal44@yahoo.com>
2022-12-06 23:35:08 +0100

    import genre

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectGenreListId } from "../redux/features/playerSlice";
 import { Error, Loader, SongCard } from "../components";
-e
+import { genres } from "../assets/constants";
+// import { useGetSongsByGenreQuery } from "../redux/services/shazamCore"; now we want to use the Genre
 
 
 
