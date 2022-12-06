14d44a389d8ae9b03fffda51713cf427b5548581
move style to head <timmortal44@yahoo.com>
2022-12-06 23:39:01 +0100

    make h2 tAG

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -32,7 +32,9 @@ const { activeSong, isPlaying, genreListId } = useSelector((state) => state.play
         <div className="flex flex-col">
             {/* he's using a tailwind extension for seeing what eachtail wind property does */}
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className="font-bold text-3xl text-white text-left">
                 
+                    </h2>
                   
             </div>      
 
