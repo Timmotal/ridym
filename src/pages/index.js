2d7b2a09239151e23a0f4b8fa2ceeafe394798e2
move style to head <timmortal44@yahoo.com>
2022-12-06 23:49:56 +0100

    imp around you

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import ArtistDetails from './ArtistDetails';
 import SongDetails from './SongDetails';
 import Search from './Search';
 import TopCharts from './TopCharts';
-
+import AroundYou from './AroundYou';
 
 export {
   Discover,
