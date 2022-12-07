5d440b8858a32ff4da011581063a7b4e94c9ba51
move style to head <timmortal44@yahoo.com>
2022-12-07 23:10:11 +0100

    import error comp

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,5 @@
 import { useParams } from "react-router-dom"; // we are getting the search term from the URl
 import axios from "axios";
 import { useSelector } from "react-redux";
+
+import { Error, Loader, SongCard } from "../components";
