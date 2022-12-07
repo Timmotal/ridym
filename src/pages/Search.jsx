33a2314296a7ac816902a22a9dfece8f261e6989
move style to head <timmortal44@yahoo.com>
2022-12-07 23:12:59 +0100

    make h2

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -23,7 +23,10 @@ const Search = () => {
 
     return (
         <div className="flex flex-col">
-          
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+           
+            </h2>
+
 
         </div>
     )
