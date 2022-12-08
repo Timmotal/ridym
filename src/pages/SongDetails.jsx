ddd6094bb6c0d54c1cccfd55ea2820827947a06f
move style to head <timmortal44@yahoo.com>
2022-12-08 23:50:07 +0100

    pass in props to details header;

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -33,7 +33,7 @@ const SongDetails = () => {
 
     return (
         <div className="flex flex-col">
-            <DetailsHeader  />
+            <DetailsHeader artistId=""  />
             
 
         </div>
