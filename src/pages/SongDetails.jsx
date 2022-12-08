75308683aa320924f64c98258f59654171db6afc
move style to head <timmortal44@yahoo.com>
2022-12-08 23:55:52 +0100

    add another div

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -42,7 +42,7 @@ const SongDetails = () => {
                     <div className="mt-5">
                         {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
                             <p className="text-gray-400 text-base my-1">{line}</p>
-                        )) : }
+                        )) : <p className="text-gray-400 text-base my-1"></p>}
                     </div>
                 </div>
 
