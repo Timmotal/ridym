2a1e9fcb724d33ca10566bbb08ca38cb6a2a45cb
move style to head <timmortal44@yahoo.com>
2022-12-08 23:55:15 +0100

    add p tag

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -41,7 +41,7 @@ const SongDetails = () => {
 
                     <div className="mt-5">
                         {songData?.sections[1].type === 'LYRICS' ? songData?.sections[1].text.map((line, i) => (
-                            
+                            <p className="text-gray-400 text-base my-1">{line}</p>
                         )) : }
                     </div>
                 </div>
