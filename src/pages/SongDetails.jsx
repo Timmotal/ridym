5b75b801c730cbc16a65896e4f390c6c3173971d
move style to head <timmortal44@yahoo.com>
2022-12-08 23:38:55 +0100

    import deatailsHeader, error, loader, relatedSongs from components

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom"; 
+import { useParams } from "react-router-dom"; //this gives us access to the id of the url
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
 
