c75b75227b0f5e35980ad05e37fb4fb9173d6419
move style to head <timmortal44@yahoo.com>
2022-12-08 23:39:29 +0100

    import useGetSongQuery

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,6 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom"; //this gives us access to the id of the url
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
+
+//SO MUCH TO BREAKDOWN AND UNDERSTAND HERE
+
 
 
 
