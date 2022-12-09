4f5b42256a6d9e4d8b9490ca26a24084fe3ba9f6
move style to head <timmortal44@yahoo.com>
2022-12-09 23:57:03 +0100

    add another div

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -15,7 +15,9 @@ const TopArtists = () => {
             <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
             hoty hotyy in your blocks
             </h2>
-
+<div className="flex flex-wrap sm:justify-start justify-center gap-8">
+   
+</div>
 
         </div>
     )
