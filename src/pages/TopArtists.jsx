acee3bac29ce935375be0947b361b9209281cf8a
move style to head <timmortal44@yahoo.com>
2022-12-09 23:55:09 +0100

    make const topArtist

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,5 +1,16 @@
 import { Error, Loader, ArtistCard } from "../components";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
+const TopArtists = () => {
+
+    
+
+ 
+
+    //if there's an error and country is not an empty string
+  
+
+    
+};
 
 
