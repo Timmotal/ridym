3d8226ebb7de0e5649f38792871b65b06fb8d3e4
move style to head <timmortal44@yahoo.com>
2022-12-09 23:56:45 +0100

    add h2 tagfs

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -12,7 +12,9 @@ const TopArtists = () => {
 
     return (
         <div className="flex flex-col">
-           
+            <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+       
+            </h2>
 
 
         </div>
