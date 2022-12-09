e2ee40d624c0a68040c080e5135306c457a47e07
move style to head <timmortal44@yahoo.com>
2022-12-09 23:57:26 +0100

    add artistCard component

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -17,7 +17,10 @@ const TopArtists = () => {
             </h2>
 <div className="flex flex-wrap sm:justify-start justify-center gap-8">
     {data?.map((track) => (
+        <ArtistCard // feel the power and re-useability of components in react
+        
        
+        />
     ))}
 </div>
 
