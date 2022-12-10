b2a4562e4f2f7239fb169642895d260158ce91af
move style to head <timmortal44@yahoo.com>
2022-12-11 00:02:55 +0100

    remove comments

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,21 +1,16 @@
-// import { useState, useEffect} from "react"; we wont be needing the useStates, why so bro?
 import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
-// import { useContext } from 'react';
 
 const TopCharts = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
-    // tutor says we can see how simple this is because we were using redux toolkit 
-    // thereby allowing us to make those calls and wrote proper reuseable components
     const { data, isFetching, error } = useGetTopChartsQuery();
 
 
     if(isFetching) return <Loader title="loadin din din loading" />
 
-    //if there's an error and country is not an empty string
     if(error) return <Error />;
 
     return (
@@ -25,7 +20,7 @@ const TopCharts = () => {
             </h2>
 <div className="flex flex-wrap sm:justify-start justify-center gap-8">
     {data?.map((song, i) => (
-        <SongCard // feel the power and re-useability of components in react
+        <SongCard 
             key={song.key}
             song={song}
             isPlaying={isPlaying}
