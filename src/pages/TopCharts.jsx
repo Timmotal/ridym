bb52cc1e3c5950837308153dc8ae16ea2da883b2
move style to head <timmortal44@yahoo.com>
2022-12-10 23:58:14 +0100

    import useGetChartsQuery

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -3,5 +3,7 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+// import { useContext } from 'react';
 
 
