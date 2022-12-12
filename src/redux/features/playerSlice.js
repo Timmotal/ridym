b2ffe26f914fb3fbb0ae41b5fe6bb4a723c96d78
move style to head <timmortal44@yahoo.com>
2022-12-12 23:38:36 +0100

    make curret songs to action payload

--- src/redux/features/playerSlice.js
+++ src/redux/features/playerSlice.js
@@ -21,7 +21,7 @@ const playerSlice = createSlice({
       } else if (action.payload?.data?.properties) {
         state.currentSongs = action.payload?.data?.tracks;
       } else {
-      
+        state.currentSongs = action.payload.data;
       }
 
   
