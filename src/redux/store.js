90d9f9943580d58b548271331dbefde4c29ae7d0
move style to head <timmortal44@yahoo.com>
2022-12-13 23:46:50 +0100

    include reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,5 +4,9 @@ import playerReducer from './features/playerSlice';
 import { shazamCoreApi } from './services/shazamCore';
 
 export const store = configureStore({
-  
+  reducer: {
+    
+   
+  },
+
 });
