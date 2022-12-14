a4e1e555f93cf6f4459510eae9b6730b7b5f54f9
move style to head <timmortal44@yahoo.com>
2022-12-14 23:56:55 +0100

    createApi and fetchBaseQuery

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// const options = {
+//     method: 'GET',
+//     headers: { 
+//       'X-RapidAPI-Key': '1beaee4af1msh347530e5422fc35p11ada2jsnbf03d4904b5e',
+//       'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+//     }
+//   };
+  
+//   fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+//     .catch(err => console.error(err));
+
+    
+
+   
+
+    
