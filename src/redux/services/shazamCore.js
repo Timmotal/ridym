98ce1e2bf715cdc8f4ce6592d9f277c207ede907
move style to head <timmortal44@yahoo.com>
2022-12-15 08:04:40 +0100

    getSongsSearch in endpoint

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -32,7 +32,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
         // why destructure sometimes and why did we pass it without destructuring here
         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
         getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
-        s 
+        getSongsBySearch: builder.query({ query: (searchTerm) => `search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`})
+        // tutor says i think you can now see the power of creating a proper Redux Toolkit Structure
+        // it's so easy to add new APi endpoints 
     }),
      });
     
