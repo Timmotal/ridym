185362e9fb94037a75c1b47d3dc7517659fdf10f
move style to head <timmortal44@yahoo.com>
2022-12-15 08:03:19 +0100

    add endpoints

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -23,7 +23,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
             return headers;
         },
     }),
-   
+    endpoints: (builder) => ({  
+        
+    }),
      });
     
 
