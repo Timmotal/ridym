e76c443826b7c5ddc6a05477471750319aeb0f2c
move style to head <timmortal44@yahoo.com>
2022-12-16 23:32:44 +0100

    include top charts

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -21,5 +21,5 @@ export const links = [
   { name: 'Discover', to: '/', icon: HiOutlineHome },
   { name: 'Around You', to: '/around-you', icon: HiOutlinePhotograph },
   { name: 'Top Artists', to: '/top-artists', icon: HiOutlineUserGroup },
- 
+  { name: 'Top Charts', to: '/top-charts', icon: HiOutlineHashtag },
 ];
