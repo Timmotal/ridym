88fa1d465bd4203487e0f9240d2d48cb49f28bbe
move style to head <timmortal44@yahoo.com>
2022-12-17 23:52:28 +0100

    add slideleft to animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       animation: {
         slideup: 'slideup 1s ease-in-out',
         slidedown: 'slidedown 1s ease-in-out',
-     
+        slideleft: 'slideleft 1s ease-in-out',
      
       
       },
