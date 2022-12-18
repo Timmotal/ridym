512167cf230e08e14b3b7a35bcc5cd67446615cf
move style to head <timmortal44@yahoo.com>
2022-12-18 23:11:15 +0100

    add wave

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,11 @@ module.exports = {
           from: { opacity: 0, transform: 'translateX(20px)' },
           to: { opacity: 1, transform: 'translateX(0)' },
         },
+        wave: {
+          
        
+ 
+        },
       },
     },
   },
