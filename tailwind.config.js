d49e1c1cc6812f192b3779b2b33a2d523c00b94f
move style to head <timmortal44@yahoo.com>
2022-12-18 23:09:15 +0100

    add from to slide of key frames

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           to: { opacity: 1 },
         },
         slideup: {
-        
+          from: { opacity: 0, transform: 'translateY(25%)' },
         
         },
         
