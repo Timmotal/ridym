67265b23632d93f5751df2b41026cf4fa6f50639
move style to head <timmortal44@yahoo.com>
2022-12-18 23:10:48 +0100

    add slideer right

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,11 @@ module.exports = {
           from: { opacity: 0, transform: 'translateX(-20px)' },
           to: { opacity: 1, transform: 'translateX(0)' },
         },
-        
+        slideright: {
+          
+         
+        },
+       
       },
     },
   },
