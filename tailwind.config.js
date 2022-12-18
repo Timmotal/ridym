229f076112a775bee67db31075a9bceb882d2428
move style to head <timmortal44@yahoo.com>
2022-12-18 23:10:16 +0100

    add slide left to key frames

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,10 @@ module.exports = {
           from: { opacity: 0, transform: 'translateY(-25%)' },
           to: { opacity: 1, transform: 'none' },
         },
-       
+        slideleft: {
+         
+          
+        },
         
       },
     },
