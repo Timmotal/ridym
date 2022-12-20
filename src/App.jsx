a35040467f518eeaa9356a29a962ff8a2dedebbc
move style to head <timmortal44@yahoo.com>
2022-12-20 23:38:42 +0100

    include topPlay

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
             </Routes>
           </div>
           <div className="xl:sticky relative top-0 h-fit">
-            <
+            <TopPlay /> 
           </div>
         </div>
       </div>
