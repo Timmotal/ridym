ef68c4e4d5c8cdd764bd269fb200d0d26fdb5c38
move style to head <timmortal44@yahoo.com>
2022-12-20 23:33:35 +0100

    add artistDetails

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
-
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
 
 
