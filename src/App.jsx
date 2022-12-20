fd657916d00e600a4c8c9475f43f6e5f2ca58a9e
move style to head <timmortal44@yahoo.com>
2022-12-20 23:36:03 +0100

    include top artist route

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,14 @@ const App = () => {
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
-              <Route path="/" element={<Discover />} /> </Routes>
+              <Route path="/" element={<Discover />} />
+              <Route path="/top-artists" element={<TopArtists />} />
+          
+              
+             
+             
+             
+            </Routes>
           </div>
           
         </div>
