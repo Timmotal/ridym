95577db1208f250b9e1bd866089eb1660ff7c883
move style to head <timmortal44@yahoo.com>
2022-12-20 23:38:07 +0100

    include artistId

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-charts" element={<TopCharts />} />
               <Route path="/around-you" element={<AroundYou />} />
-             
+              <Route path="/artists/:id" element={<ArtistDetails />} />
              
              
             </Routes>
