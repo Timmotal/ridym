30372db66b9ced0e732565b682f1be8c59d05964
move style to head <timmortal44@yahoo.com>
2022-12-20 23:33:45 +0100

    make App function

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
+const App = () => {
+
+
+  
+};
 
 
