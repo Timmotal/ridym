d32f0dcd49db9f0935716518eed694d646a43316
move style to head <timmortal44@yahoo.com>
2022-12-20 23:33:57 +0100

    init activeSong

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
 const App = () => {
-
+  const { activeSong } = useSelector((state) => state.player);
 
   
 };
