c9a504d5f6e9d36e2d32ba50e0079219ffa344a1
move style to head <timmortal44@yahoo.com>
2023-04-27 23:23:09 +0100

    complete new error message

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 const Error = () => (
   <div className="w-full flex justify-center items-center">
     <h1 className="font-bold text-2xl text-white mt-2">
-      Please try again, there are some fragalistas interring with.
+      Please try again, there are some fragalistas interring with the transmission.
     </h1>
   </div>
 );
