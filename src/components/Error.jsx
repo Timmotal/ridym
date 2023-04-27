b29267ac46b135611ff9c87fe4e680e9f378383d
move style to head <timmortal44@yahoo.com>
2023-04-27 23:22:51 +0100

    include new word

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 const Error = () => (
   <div className="w-full flex justify-center items-center">
     <h1 className="font-bold text-2xl text-white mt-2">
-      Please try again, there are some fragalistas around here.
+      Please try again, there are some fragalistas interring with.
     </h1>
   </div>
 );
