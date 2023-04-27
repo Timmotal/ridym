98251290121c33cacd9476b573fa429c95e86060
move style to head <timmortal44@yahoo.com>
2023-04-27 23:21:44 +0100

    adjust code comment

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => {
 
-  const artist = artistData?.artists[artistId].attributes; // so to reduce the long string and make it data simpler we did this
+  const artist = artistData?.artists[artistId].attributes; 
+  // so to reduce the long string and make it data simpler we did this
 
   return (
   <div className="relative w-full flex flex-col">
