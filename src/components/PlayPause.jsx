208300ce216381fbb9db453bb70c99aa5d9ea8c7
Timmortal <43627445+Timmotal@users.noreply.github.com>
2023-11-05 11:51:46 +0000

    Update PlayPause.jsx

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,4 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
-// why simple handlePause and not handlePauseClick
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => // if i had traced the props maybe i would have figured
                       ( isPlaying && activeSong?. title === song.title ? // if we are playing and active song is equal to a particular song
                         ( <FaPauseCircle
